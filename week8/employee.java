ca4848cf167846bfad5b7d6521d8c08fe66baf66
aryusharyal-collab <aryusharyal@gmail.com>
2025-12-23 12:47:21 +0545

    p

---
+++ week8/employee.java
@@ -0,0 +1,16 @@
+package week8;
+
+
+/**
+ * Write a description of class employee here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class employee
+{
+   private int id;
+   private String name;
+   private double salary;
+   
+}
